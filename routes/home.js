84a09e523d6971e211a8e9cafaa284fdb2032392
mgmoonlight <mg.moonlight@gmail.com>
2016-03-24 13:59:17 +0200

    First commit

---
+++ routes/home.js
@@ -0,0 +1,19 @@
+module.exports = function (app) {
+
+    // home page
+    app.get('/', function (req, res) {
+        res.render('index', { title: 'Home Page.  ' })
+    });
+
+    // chat area
+    app.get('/chat', function (req, res) {
+        res.render('chat', { title: 'Chat with Me!  ' })
+    });
+
+    // about page
+    app.get('/about', function (req, res) {
+        //res.render('about', { title: 'About Me.  ' })
+        res.render('index.html')
+         //res.render('./public/index.html');
+    });
+}
